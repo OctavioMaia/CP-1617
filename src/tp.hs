1910406cdb61fed233e712eb2dccf7098a26693a
OctavioMaia <you@example.com>
2017-06-13 15:07:00 +0100

    rm cenas

--- src/tp.hs
+++
@@ -1,8 +0,0 @@
-inv :: Double -> Double -> Double
-inv x 0 = 1
-inv x 1 = (1-x)
-inv x n = (conta (1-x) n) + inv x (n-1)
-
-conta :: Double -> Double -> Double
-conta x 0 = 1
-conta x n = x * (conta x (n-1)) 
